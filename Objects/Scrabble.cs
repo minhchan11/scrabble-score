b6da4d7f2d0f0948c7315b0b79c7a48edfc09cdb
Minh Phuong and Tammy Dang <minh+tammy@>
2017-02-16 09:05:45 -0800

    Add method to split words and to verify if there is a Testing with Xunit

--- Objects/Scrabble.cs
+++ Objects/Scrabble.cs
@@ -1,11 +1,44 @@
-using Systems;
-using Systems.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ScrabbleGame
 {
   public class Scrabble
   {
-    private string input
-    
+    private string _inputWords;
+    public List<char> _inputList = new List<char>{};
+    public int _testNo;
+
+    public Scrabble(string inputWords)
+    {
+      _inputWords = inputWords;
+    }
+
+    public List<string> SplitWords()
+    {
+      char[] inputArray = _inputWords.ToCharArray();
+
+      foreach (char letter in inputArray)
+      {
+        _inputList.Add(letter);
+      };
+
+      var result = _inputList.Select(c => c.ToString()).ToList();
+      return result;
+    }
+
+    public bool ContainsA(List<string> result)
+    {
+      foreach(string letter in result)
+      {
+        if(letter == "a")
+        {
+        _testNo =1;
+        }
+      }
+      if(_testNo == 1);
+      { return true ;}
+    }
   }
 }
--- Tests/ScrabbleTest.cs
+++ Tests/ScrabbleTest.cs
@@ -1,3 +1,33 @@
-using Systems;
-using Systems.Collections.Generic;
-using xUnit;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ScrabbleGame
+{
+  public class ScrabbleTest
+  {
+  [Fact]
+  public void SplitWords_UsersInputWords_SplittedCharacters()
+  {
+    //Arrange
+    string inputWords = "dog";
+    Scrabble testScrabble = new Scrabble(inputWords);
+    List<string> outputString = testScrabble.SplitWords();
+    //Act
+    List<string> verifyString = new List<string> {"d","o","g"};
+    //Assert
+    Assert.Equal(outputString,verifyString);
+  }
+  [Fact]
+  public void SplitWords_UsersInputWords_true()
+  {
+    //Arrange
+    string inputWords = "cat";
+    Scrabble testScrabble = new Scrabble(inputWords);
+    List<string> outputString = testScrabble.SplitWords();
+    //Assert
+    Assert.Equal(true,testScrabble.ContainsA(outputString));
+  }
+
+  }
+}
